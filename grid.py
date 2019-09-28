59b8c6cb7e4bb5ca7e97f53960a799207f5d0251
shreyaj98 <32920257+shreyaj98@users.noreply.github.com>
2019-09-28 10:16:25 -0400

    Update grid.py

--- grid.py
+++ grid.py
@@ -1,1 +1,30 @@
-print("hello world")
+import random
+
+def drawGrid():
+    grid = []
+    for rows in range(101):
+        grid.append([])
+        for cols in range(101):
+            grid[rows].append(0)
+    return grid
+
+def startState(grid):
+    x = random.randint(0, len(grid))
+    y = random.randint(0, len(grid))
+    grid[x][y] = 's'
+
+def goalState(grid):
+    x = random.randint(0, len(grid))
+    y = random.randint(0, len(grid))
+    grid[x][y] = 'x'
+
+
+def createBlocks(grid):
+    for x in range(len(grid)):
+        for y in range(len(grid)):
+            if(random() > 0.7):
+                #blocked
+                grid[x][y] = 'b'
+            else:
+                #open
+                grid[x][y] = 'o'
